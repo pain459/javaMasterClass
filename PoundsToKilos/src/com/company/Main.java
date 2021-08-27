f4efc3d1cb96f43df1719f581a29276662752a9d
Ravi <ravi.ameer@gmail.com>
2021-08-27 18:17:18 +0530

    Added exercise to convert pounds to kilos.

---
+++ PoundsToKilos/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // This program will convert pounds to kilograms.
+        double weightInPounds = 200D;
+        double weightInKilograms = weightInPounds * 0.45359237D;
+        System.out.println(weightInPounds + " pounds converted into kilograms is " + weightInKilograms + " KG.");
+    }
+}
