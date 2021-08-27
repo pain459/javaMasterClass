bd7e48cdb37d8df899eda4e8bc0edb3c296a0e57
Ravi <ravi.ameer@gmail.com>
2021-08-27 20:03:58 +0530

    Char and boolean data types with examples.

---
+++ CharAndBoolean/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char myChar = 'D';  // Only stores one character.
+        char uniCodeChar = '\u0C05';
+        System.out.println(uniCodeChar);
+        char chakraSign = '\u0995';
+        System.out.println(chakraSign);
+        boolean myTrueBoolValue = true;
+        boolean myFalseBoolValue = false;
+
+    }
+}
