ae29bb0ce2fbbc381a119d882009e8fbe5ea673b
Ravi <ravi.ameer@gmail.com>
2021-08-27 17:50:00 +0530

    Concluding the Float and Double practice code.

--- FloatAndDouble/src/com/company/Main.java
+++ FloatAndDouble/src/com/company/Main.java
@@ -12,9 +12,12 @@ public class Main {
         System.out.println("Min double value = " + myMinDoubleValue);
         System.out.println("Max double value = " + myMaxDoubleValue);
 
-        int myValueInt = 5;
-        float myValueFloat = 5.25F;  // declaring the type of variables is a good practice.
-        double myValueDouble = 5D;
+        int myValueInt = 5 / 3;
+        float myValueFloat = 5F / 3F;  // declaring the type of variables is a good practice.
+        double myValueDouble = 5.00 / 3.00; // JAVA uses double by default for floating point numbers. So, no need to specify this again. Going forward in this course, we will only double.
         float testCastingVar = (float) 5.25;  // We can cast the value like this.
+        System.out.println("My Int value = " + myValueInt);
+        System.out.println("My Float value = " + myValueFloat);
+        System.out.println("My Double value = " + myValueDouble);
     }
 }
