d92efc1a9e3555815e01dd04c5334262b78ae0ed
Ravi <ravi.ameer@gmail.com>
2021-08-27 13:23:58 +0530

    Added projects and basic lessons.

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int myFirstNumber = 5; // declarative statement. Assigning a literal value.
+        System.out.println(myFirstNumber); //printing out the variable.
+    }
+}
